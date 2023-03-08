c6998935fea2bfef7031118a833e7a83582da152
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-03-08 20:35:48 +0300

    first draft without main

---
+++ src/by/teachmeskills/dzeviatsen/homework18/IntLinkedList.java
@@ -0,0 +1,90 @@
+package by.teachmeskills.dzeviatsen.homework18;
+
+public class IntLinkedList implements IntList {
+    private IntLinkedNode headNode;
+
+    public IntLinkedList() {
+        headNode = null;
+    }
+
+    @Override
+    public int get(int index) {
+        int counter = 0;
+        while (counter != index) {
+            if (headNode.getNextNode() == null) {
+                throw new IllegalArgumentException("Index out of bounds");
+            }
+            headNode.getNextNode();
+            counter++;
+        }
+        return headNode.getElement();
+    }
+
+    @Override
+    public int set(int index, int element) {
+        if (index > size() || index < 0) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+        int returnedElement = 0;
+        for (int i = 0; i <= index; i++) {
+            if (i == index) {
+                headNode.getNextNode();
+                returnedElement = headNode.getElement();
+                headNode.setElement(element);
+            }
+        }
+        return returnedElement;
+    }
+
+    @Override
+    public int size() {
+        int size = 0;
+        while (headNode.getNextNode() != null) {
+            size = size + 1;
+        }
+        return size;
+    }
+
+    @Override
+    public void add(int element) {
+        IntLinkedNode node = new IntLinkedNode();
+        node.setElement(element);
+        if (headNode == null) {
+            headNode = node;
+        } else {
+            while (headNode.getNextNode() != null) {
+                headNode = headNode.getNextNode();
+            }
+            headNode.getNextNode().setNextNode(node);
+        }
+    }
+
+    public int remove(int index) {
+        int removedNum = 0;
+        if (index < 0 || index > size()) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+        for (int i = 0; i <= index; i++) {
+            headNode.getNextNode();
+            if (i + 1 == index) {
+                removedNum = headNode.getElement();
+                headNode.setNextNode(headNode.getNextNode().getNextNode());
+                headNode = headNode.getNextNode();
+                return removedNum;
+            }
+        }
+        return removedNum;
+    }
+
+    public int lastIndexOf(int element) {
+        int lastIndex = 0;
+        for (int i = 0; i < size(); i++) {
+            headNode.getNextNode();
+            if (headNode.getElement() == element) {
+                lastIndex = i;
+            }
+            headNode=headNode.getNextNode();
+        }
+        return lastIndex;
+    }
+}
---
+++ src/by/teachmeskills/dzeviatsen/homework18/IntLinkedNode.java
@@ -0,0 +1,22 @@
+package by.teachmeskills.dzeviatsen.homework18;
+
+public class IntLinkedNode {
+    private int element;
+    private IntLinkedNode nextNode;
+
+    public int getElement() {
+        return this.element;
+    }
+
+    public void setElement(int element) {
+        this.element = element;
+    }
+
+    IntLinkedNode getNextNode() {
+        return this.nextNode;
+    }
+
+    public void setNextNode(IntLinkedNode nextNode) {
+        this.nextNode = nextNode;
+    }
+}
